11b72572d812345c4770c200eff9a91fbd2d812d
Colorfuls <mint.rosetta2001@gmail.com>
2024-03-23 18:29:20 +0700

    lecture - Spring Process Behind

--- projects/02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/CricketCoach.java
+++ projects/02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/CricketCoach.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements ICoach {
-
 	@Override
 	public String getDailyWorkout() {
 		return "Practice fast bowling for 15 minute. !!!!!";
 	}
-
 }
