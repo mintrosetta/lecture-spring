651a5838a3ada1e8102b823c5537b38390766207
Colorfuls <mint.rosetta2001@gmail.com>
2024-03-23 19:29:33 +0700

    lecture - component scanning

---
+++ projects/02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.luv2code.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.util.ICoach;
+
+@RestController
+public class DemoController {
+	// define a private field for the dependency
+	private ICoach coach;
+	
+	// define a constructor for dependency injection
+	@Autowired // if only have one constructor, Autowired is optional
+	public DemoController(ICoach coach) {
+		this.coach = coach;
+	}
+	
+	@GetMapping("/dailyworkout")
+	public String getDailyWorkout() {
+		return this.coach.getDailyWorkout();
+	}
+}
