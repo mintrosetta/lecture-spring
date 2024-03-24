7fb7a9f9216dc0ec34a2bb3969ee9d4c6d8956de
Colorfuls <mint.rosetta2001@gmail.com>
2024-03-24 12:52:50 +0700

    lecture - primary annotation

---
+++ projects/02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springcoredemo.common.ICoach;
+
+@RestController
+public class DemoController {
+	// define a private field for the dependency
+	private ICoach coach;
+	
+	@Autowired
+	public void setCoach(ICoach coach) {
+		this.coach = coach;
+	}
+	
+	@GetMapping("/dailyworkout")
+	public String getDailyWorkout() {
+		return this.coach.getDailyWorkout();
+	}
+}
