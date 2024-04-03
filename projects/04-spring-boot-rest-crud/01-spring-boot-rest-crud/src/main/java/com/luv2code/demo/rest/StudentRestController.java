ba9ea1dd3c19ee8ad148c5fde39baea9610ee84f
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-04-04 00:39:20 +0700

    lecture - get single student

--- projects/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ projects/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -4,24 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.demo.entities.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// defint endpoint for '/students' - return list of student
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<Student>();
-		students.add(new Student("Bike", "Chanokchon"));
-		students.add(new Student("Mint", "Colorfuls"));
-		students.add(new Student("Mint", "Rosetta"));
+	private List<Student> students;
+	
+	// define @PostConstruct to load students data
+	@PostConstruct
+	public void loadData() {
+		this.students = new ArrayList<Student>();
 		
-		return students;
+		this.students.add(new Student("Bike", "Chanokchon"));
+		this.students.add(new Student("Mint", "Colorfuls"));
+		this.students.add(new Student("Mint", "Rosetta"));
 	}
 	
+	// defint endpoint for '/students' - return list of student
+	@GetMapping("/students")
+	public List<Student> getStudents() {		
+		return this.students;
+	}	
+	
+	// define endpoint for 'students/{studentId}' - return single student
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return this.students.get(studentId); // use for get data from index
+	}
 }
