49d9c042d5b70ed82f3fe3826faa94b8ec60de63
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-04-08 10:55:10 +0700

    add remove employee by id

--- projects/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ projects/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,16 @@ public class EmployeeRestController {
 		Employee dbEmployee = this.employeeService.save(employee);
 		
 		return dbEmployee;
+	}
+	
+	@DeleteMapping("/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		Employee employee = this.employeeService.findById(employeeId);
+		
+		if (employee == null) throw new RuntimeException("Employee id not found - " + employeeId);
+		
+		this.employeeService.removeById(employeeId);
+		
+		return "Delete employee id - " + employeeId;		
 	}
 }
