57236fd01fbfafae6832ee6785595d8995d4b769
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-04-08 10:37:48 +0700

    lecture - get employee by id

--- projects/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ projects/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,9 +1,10 @@
-package com.luv2code.springboot.cruddemo.rest;
+package com.luv2code.springboot.cruddemo.rest; 
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,14 @@ public class EmployeeRestController {
 	@GetMapping("")
 	public List<Employee> getEmployees() {
 		return this.employeeService.finds();
+	}
+	
+	@GetMapping("/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		Employee employee = this.employeeService.findById(employeeId);
+		
+		if (employee == null) throw new RuntimeException("Employee id not found - " + employeeId);
+		
+		return employee;
 	}
 }
