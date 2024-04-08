45801bc068d8ccebe5d611ecc54289877ca5a6e9
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-04-08 10:44:12 +0700

    create add employee api

--- projects/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ projects/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,15 @@ public class EmployeeRestController {
 		if (employee == null) throw new RuntimeException("Employee id not found - " + employeeId);
 		
 		return employee;
+	}
+	
+	@PostMapping("")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		// in this case, pass id in json to 0 for save new item
+		employee.setId(0);
+		
+		Employee dbEmployee = this.employeeService.save(employee);
+		
+		return dbEmployee;
 	}
 }
