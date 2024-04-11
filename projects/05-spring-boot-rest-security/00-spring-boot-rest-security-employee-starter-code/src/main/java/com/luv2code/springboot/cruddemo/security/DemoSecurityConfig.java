508ad72bb0eb9c40781d46f3415b6b41a6dabec9
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-04-11 22:39:03 +0700

    lecture: setup security using jdbc

--- projects/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ projects/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,50 +1,36 @@
 package com.luv2code.springboot.cruddemo.security;
 
-import java.util.List;
-import java.util.ArrayList;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
+	// add support for jdbc ... no more hardcoded user
 	@Bean
-	public InMemoryUserDetailsManager userDetailManager() {
-		UserDetails jhon = User.builder().username("jhon").password("{noop}test123").roles("EMPLOYEE").build();
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-		UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER")
-				.build();
-
-		UserDetails susan = User.builder().username("susan").password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-		List<UserDetails> users = new ArrayList<UserDetails>();
-		users.add(jhon);
-		users.add(mary);
-		users.add(susan);
-
-		return new InMemoryUserDetailsManager(users);
+		// tell spring security to use jdbc authentication with our data source
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		String baseEmployeeAPI = "/api/employees";
 
-		http.authorizeHttpRequests(config -> 
-			config.requestMatchers(HttpMethod.GET, baseEmployeeAPI).hasRole("EMPLOYEE")
-			.requestMatchers(HttpMethod.GET, baseEmployeeAPI + "/**").hasRole("EMPLOYEE")
-			.requestMatchers(HttpMethod.POST, baseEmployeeAPI).hasRole("MANAGER")
-			.requestMatchers(HttpMethod.PUT, baseEmployeeAPI).hasRole("MANAGER")
-			.requestMatchers(HttpMethod.PUT, baseEmployeeAPI + "/**").hasRole("MANAGER")
-			.requestMatchers(HttpMethod.DELETE, baseEmployeeAPI + "/**").hasRole("ADMIN")
-		);
+		http.authorizeHttpRequests(config -> config.requestMatchers(HttpMethod.GET, baseEmployeeAPI).hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, baseEmployeeAPI + "/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, baseEmployeeAPI).hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, baseEmployeeAPI).hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, baseEmployeeAPI + "/**").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, baseEmployeeAPI + "/**").hasRole("ADMIN"));
 
 		// tell spring, use basic authentication
 		http.httpBasic(Customizer.withDefaults());
@@ -55,3 +41,25 @@ public class DemoSecurityConfig {
 		return http.build();
 	}
 }
+
+// @Bean
+// public InMemoryUserDetailsManager userDetailManager() {
+// UserDetails jhon =
+// User.builder().username("jhon").password("{noop}test123").roles("EMPLOYEE").build();
+
+// UserDetails mary =
+// User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE",
+// "MANAGER")
+// .build();
+
+// UserDetails susan =
+// User.builder().username("susan").password("{noop}test123")
+// .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+// List<UserDetails> users = new ArrayList<UserDetails>();
+// users.add(jhon);
+// users.add(mary);
+// users.add(susan);
+
+// return new InMemoryUserDetailsManager(users);
+// }
