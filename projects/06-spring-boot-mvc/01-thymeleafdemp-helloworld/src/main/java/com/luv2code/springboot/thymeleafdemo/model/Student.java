2e1ccc596a33ad1352c7514e772f24741d78afcf
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-04-20 16:05:55 +0700

    lecture - dropdown with thymeleaf

--- projects/06-spring-boot-mvc/01-thymeleafdemp-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ projects/06-spring-boot-mvc/01-thymeleafdemp-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.thymeleafdemo.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student() {
         
@@ -23,4 +24,13 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
 }
