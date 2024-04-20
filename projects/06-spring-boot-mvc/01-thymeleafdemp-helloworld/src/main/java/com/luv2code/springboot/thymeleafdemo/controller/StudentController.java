b31fdcc3e1ed835842e883a9566fb93fab168f98
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-04-20 15:53:27 +0700

    lecture - create user with confirmation

--- projects/06-spring-boot-mvc/01-thymeleafdemp-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ projects/06-spring-boot-mvc/01-thymeleafdemp-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
@@ -20,6 +22,15 @@ public class StudentController {
         model.addAttribute("student", student);
 
         return "student-create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute("student") Student student) {
+        // log the input data
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+
+        return "student-confirmation";
     }
 
 }
