be2746cdd8e5b8a7e208996b2fbd5c0fb266ae1b
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-04 16:43:14 +0700

    lecture - add Review

---
+++ projects/09-hibernate-jpa-advance/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,26 @@
+package com.luv2code.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.luv2code.cruddemo.dao.AppDAO;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	// executed after the Spring Bean have loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		//Java lampda expression
+		return runner -> {
+
+		};
+	}
+
+}
